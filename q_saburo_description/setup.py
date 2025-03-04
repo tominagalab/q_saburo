4fcb44ac1d663a91ed48064059a6996b23437177
docker foxy <tommy.tommy.walker@gmail.com>
2025-03-04 12:25:15 +0000

    done tutorial

--- q_saburo_description/setup.py
+++ q_saburo_description/setup.py
@@ -1,16 +1,27 @@
+import os
+from glob import glob
 from setuptools import setup
 
 package_name = 'q_saburo_description'
+
+data_files = []
+data_files.append(('share/ament_index/resource_index/packages', ['resource/' + package_name]))
+data_files.append(('share/' + package_name, ['package.xml']))
+
+def package_files(directory, data_files):
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            data_files.append(('share/' + package_name + '/' + path, glob(path + '/**/*.*', recursive=True)))
+    return data_files
+
+data_files = package_files('launch/', data_files)
+data_files = package_files('urdf/', data_files)
 
 setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+    data_files=data_files,
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='ubuntu',
